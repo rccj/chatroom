9720aa7438a983a4fc928c61b0c90f0bc287f5f4
Roman Chen <roman.c.tcg@gmail.com>
2025-02-15 18:36:48 +0800

    chore: add shadcn for ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button"
 function App() {
 	return (
 		<div>
 			<div className="flex flex-col items-center justify-center h-screen">
-				<button className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">Join Room</button>
+				<Button>Join Room</Button>
 				<h1 className="text-4xl font-bold">Chat Room</h1>
 			</div>
 		</div>
